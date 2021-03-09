acccf0d93f21bb2bad6241da4ec7f1556ac67f5b
Tosia <antoninamatuszek@gmail.com>
2021-03-09 16:13:36 +0100

    Vector class

--- Ex.1/definitions.py
+++ Ex.1/definitions.py
@@ -1,4 +1,4 @@
-class Vec(object):
+class Vec:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
@@ -6,5 +6,13 @@ class Vec(object):
 
     def __add__(self, other):
         return Vec(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __str__(self):
+       result = '(' + str(self.x) + ', ' + str(self.y) + ', ' + str(self.z) + ')'
+       return result
 
 
+ 
--- Ex.1/tests.py
+++ Ex.1/tests.py
@@ -1,1 +1,19 @@
-print ("hi")
+from definitions import * 
+import unittest
+
+
+class Test(unittest.TestCase):   
+# Vector addition test #
+    def setUp(self):
+            self.v1 = Vec(1, 2, 3)
+            self.v2 = Vec(4, 5, 6)
+
+    
+    def test_add(self):
+            self.assertEqual(self.v1 + self.v2, Vec(5,7,9))
+            self.assertNotEqual(self.v1 + self.v2, Vec(5,5,9))
+
+#print(v3.__dict__)
+
+if __name__ == "__main__":
+    unittest.main()
