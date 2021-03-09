7e3ea679377769ea51e92aa58f32fac079b38534
Tosia <antoninamatuszek@gmail.com>
2021-03-09 15:14:33 +0100

    First commit

---
+++ Ex.1/definitions.py
@@ -0,0 +1,10 @@
+class Vec(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vec(self.x + other.x, self.y + other.y, self.z + other.z)
+
+
---
+++ Ex.1/main.py
@@ -0,0 +1,9 @@
+from definitions import * 
+
+
+v1 = Vec(1, 2, 3)
+v2 = Vec(4, 5, 6)
+    
+v3 = v1 + v2
+print(v3.__dict__)
+    
