182c790e777d3350b9d10c968dda1bc42d3f15e7
Adam <adamblas97@gmail.com>
2021-03-23 14:17:34 +0100

    Added files for ex 2

---
+++ Ex.2/Primitives/plane.py
@@ -0,0 +1,49 @@
+from Primitives.ray import *
+
+
+## Documentation for a class Plane.
+class Plane:
+
+    ## Constructor.
+    def __init__(self, normal_vector, d):
+        if normal_vector == Vec3(0, 0, 0):
+            raise ValueError('Normal vector cannot be (0, 0, 0)')
+
+        ## VEctor perpendicular to plane.
+        self.normal_vector = normal_vector
+        ## Represents A in 'Ax + By + Cz D = 0' equation.
+        self.a = normal_vector.x
+        ## Represents B in 'Ax + By + Cz D = 0' equation.
+        self.b = normal_vector.y
+        ## Represents C in 'Ax + By + Cz D = 0' equation.
+        self.c = normal_vector.z
+        ## Represents D in 'Ax + By + Cz D = 0' equation.
+        self.d = d
+
+    ## Function returning object values in string format.
+    def __str__(self):
+        return str(self.a) + 'x + ' + str(self.b) + 'y + ' + str(self.c) + 'z + ' + str(self.d)
+
+    ## Checks if plane and ray intersect witch each other and returns intersection point if they do, otherwise None.
+    def get_intersection(self, ray):
+        # Ray is paralell to plane
+        if self.normal_vector * ray.direction == 0:
+            return None
+
+        # Calculate parameters
+        t = (-self.d - (self.normal_vector * ray.origin)) / (self.normal_vector * ray.direction)
+
+        # Check if intersection is before origin of ray
+        if t < 0:
+            return None
+
+        x = round(ray.origin.x + (t * ray.direction.x), 5)
+        y = round(ray.origin.y + (t * ray.direction.y), 5)
+        z = round(ray.origin.z + (t * ray.direction.z), 5)
+        point = Vec3(x, y, z)
+
+        # Check if point is in range
+        if ray.origin.distance(point) > ray.length:
+            return None
+
+        return point
---
+++ Ex.2/Primitives/ray.py
@@ -0,0 +1,88 @@
+import math
+from vector import *
+
+
+## Documentation for a class Ray.
+class Ray:
+
+    ## Constructor.
+    def __init__(self, origin=Vec3(0, 0, 0), direction=Vec3(1, 1, 1), length=math.inf):
+
+        ## Origin vector of a given ray.
+        ## Default = (0, 0, 0)
+        self.origin = origin
+        ## Direction vector of a given ray.
+        ## Cannot be (0, 0, 0).
+        ## Default = (1, 1, 1)
+        self.direction = direction / direction.length()
+        ## Length of a given ray.
+        ## Default = Infinity
+        self.length = length
+
+    ## Function returning object values in string format.
+    def __str__(self):
+        return 'Origin: ' + str(self.origin) + ', Vector: ' + str(self.direction)
+
+    ## Check if point is on ray, returns true if yes, false otherwise.
+    def is_point_on_ray(self, point):
+
+        # Variables to indicate if there is need to calculate parametric values
+        skip_x = False
+        skip_y = False
+        skip_z = False
+
+        # Check if direction value is not zero (to prevent divide by zero error) and if value is indeed zero,
+        # compare values of point and origin (if they are different, then point is not on line)
+        if self.direction.x == 0:
+            if point.x != self.origin.x:
+                return False
+            skip_x = True
+        if self.direction.y == 0:
+            if point.y != self.origin.y:
+                return False
+            skip_y = True
+        if self.direction.z == 0:
+            if point.z != self.origin.z:
+                return False
+            skip_z = True
+
+        # Get parametric values for X and Y coordinate
+        if not skip_x:
+            x_var = round((point.x - self.origin.x) / self.direction.x, 5)
+        if not skip_y:
+            y_var = round((point.y - self.origin.y) / self.direction.y, 5)
+        if not skip_z:
+            z_var = round((point.z - self.origin.z) / self.direction.z, 5)
+
+        # Check if point is on the line
+        if not ((skip_x or skip_y or x_var == y_var) and (skip_y or skip_z or y_var == z_var) and (skip_x or skip_z or x_var == z_var)):
+            return False
+
+        # Check if point is before or after origin point
+        if (not skip_x and x_var < 0) or (not skip_y and y_var < 0) or (not skip_z and z_var < 0):
+            return False
+
+        # Check if point is in range
+        if self.origin.distance(point) > self.length:
+            return False
+
+        # If all conditions are met, return true
+        return True
+
+    ## Sets new direction vector and converts it to normalized vector.
+    def set_direction(self, new_direction):
+        if new_direction == Vec3(0, 0, 0):
+            raise ValueError('Direction vector cannot be (0, 0, 0)')
+        self.direction = new_direction / new_direction.length()
+
+    ## Plane.get_intersection(ray) wrapper.
+    def get_plane_intersection(self, plane):
+        return plane.get_intersection(self)
+
+    ## Sphere.get_intersection(ray) wrapper.
+    def get_sphere_intersection(self, sphere):
+        return sphere.get_intersection(self)
+
+    ## Sphere.get_ray_intersections(ray) wrapper.
+    def get_sphere_intersections(self, sphere):
+        return sphere.get_ray_intersections(self)
