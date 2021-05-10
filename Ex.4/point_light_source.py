fe331384d4282c489624ef40c6f0cddbbca19133
Tosia <tosiaalwayssmile@gmail.com>
2021-05-10 21:30:35 +0200

    Not sure what

--- Ex.4/point_light_source.py
+++ Ex.4/point_light_source.py
@@ -1,8 +1,4 @@
 ## Documentation for a class PointLightSource. Light emitted from a Point.
 class PointLightSource(LightSource):
 
-    def getIntensityAt(self, point): 
-        distance = (self.location - point).normalize()
-        #if (distance < epsilon) :
-            #distance = epsilon
-        return 1 / (distance * distance)
+
