dd85ddc794461d1438f3e9a6e8fc1e48f65d7db1
Filip <filipgajewski4@gmail.com>
2021-05-14 14:28:32 +0200

    Add rect mapping, no tests

--- Ex.5/texture.py
+++ Ex.5/texture.py
@@ -1,17 +1,28 @@
+from PIL import Image
 
 
 ## Documentation for a class Texture.
 class Texture:
     
-    def __init__(self, height, width) -> None:
+    def __init__(self, height, width, file_name) -> None:
         
-        self.height = height
+        self.img = Image.open(file_name).convert('RGB')
 
-        self.width = width
+        self.height = self.img.shape[0]
+
+        self.width = self.img.shape[1]
 
 
-    def rectangular_mapping() -> None:
-        pass
+    def rectangular_mapping(coordinates: Vec3): 
+        u = (coordinates[2] + 1)/2
+        v = (coordinates[0] - 1)/2
+
+        x = (self.height - 1) * u
+        y = (self.width - 1) * v
+
+        r, g, b = rgb_im.getpixel((x, y))
+
+        return [r, g, b]
 
 
     def spherical_mapping() -> None:
