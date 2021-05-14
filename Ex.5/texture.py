6728be44f61d1aa5b9bf1f8d92d457bc0f1edd1d
Filip <filipgajewski4@gmail.com>
2021-05-14 14:46:08 +0200

    Add spherical mapping, no tests

--- Ex.5/texture.py
+++ Ex.5/texture.py
@@ -1,10 +1,11 @@
-from PIL import Image
+from PIL import Image, r
+import math
 
 
 ## Documentation for a class Texture.
 class Texture:
     
-    def __init__(self, height, width, file_name) -> None:
+    def __init__(self, file_name) -> None:
         
         self.img = Image.open(file_name).convert('RGB')
 
@@ -13,20 +14,34 @@ class Texture:
         self.width = self.img.shape[1]
 
 
-    def rectangular_mapping(coordinates: Vec3): 
-        u = (coordinates[2] + 1)/2
-        v = (coordinates[0] - 1)/2
+    def rectangular_mapping(self, coords: Vec3): 
+        u = (coords.z + 1)/2
+        v = (coords.x - 1)/2
 
         x = (self.height - 1) * u
         y = (self.width - 1) * v
 
-        r, g, b = rgb_im.getpixel((x, y))
+        r, g, b = self.img.getpixel((x, y))
 
         return [r, g, b]
 
 
-    def spherical_mapping() -> None:
-        pass
+    def spherical_mapping(self, coords: Vec3):
+        phi = math.atan(coords.x/coords.z)
+        theta = math.acos(coords.y)
+
+        if phi < 0:
+            phi += 2*math.pi
+
+        u = phi/(2*math.pi)
+        v = 1 - theta/math.pi
+        
+        x = (self.height - 1) * u
+        y = (self.width - 1) * v
+
+        r, g, b = self.img.getpixel((x, y))
+
+        return [r, g, b]
     
     def __str__(self) -> str:
         return 'Texture: height = ' + str(self.height) + ', width = ' + str(self.width)
